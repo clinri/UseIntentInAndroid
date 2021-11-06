7d7bb774e18a7eff1a818c43b19bb2f843730787
Andrei Borisov <clinri@ya.ru>
2021-11-06 22:37:12 +0300

    Use Intens for pass text

--- app/src/main/java/ru/clinri/useintent/MainActivity.kt
+++ app/src/main/java/ru/clinri/useintent/MainActivity.kt
@@ -1,11 +1,20 @@
 package ru.clinri.useintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickStart(view: View){
+        val intent = Intent(this,SecondActivity::class.java).apply {
+            putExtra("salut", "Привет с майн активити")
+        }
+        startActivity(intent)
     }
 }
